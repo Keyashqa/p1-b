73f0e4b1cce8a995369ee8c37f14fa44e02d14cd
Karthikeyan TS <keyashqa@gmail.com>
2025-05-14 09:53:30 +0530

    removed validators

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,9 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
-const validateRequest = require('../middleware/validateRequest');
-const { registerSchema, loginSchema } = require('../validators/userValidators');
 
-router.post('/register', validateRequest(registerSchema), registerUser);
-router.post('/login', validateRequest(loginSchema), loginUser);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 router.get('/profile', protect, getUserProfile);
 
 module.exports = router;
